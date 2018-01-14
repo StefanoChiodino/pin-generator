bca4fb63cab2a9c1ad38d753d13fedaabd750a51
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2018-01-14 11:08:19 +0000

    Randomness test

--- test/test_pinGenerator.py
+++ test/test_pinGenerator.py
@@ -9,9 +9,25 @@ class TestPinGenerator(TestCase):
         self.assertIs(type(pin), int)
 
     def test_generates_4_digits_pin(self):
-        """ "It should be 4 digits long"
-        Seen the random nature of the pin generator better run 100 iterations. """
+        """ "It should be 4 digits long" """
+        # Seen the random nature of the pin generator better run 100 iterations.
         for _ in range(0, 100):
             pin = PinGenerator.generate_pin()
             self.assertGreaterEqual(pin, 1000)
             self.assertLessEqual(pin, 9999)
+
+    def test_pin_is_random(self):
+        """ The randomness of the pin has not actually been specified in the spec but it's inferred. """
+        # The pin is in a 9999-1000=8999 range, which means in theory we could give the same twice, but chances are low.
+        # 10 attempts at getting a different pin to test randomness are given.
+        previous_pin = PinGenerator.generate_pin()
+        for _ in range(10):
+            pin = PinGenerator.generate_pin()
+            # If any of the pins were not equal then this test passes.
+            if pin != previous_pin:
+                return
+            previous_pin = pin
+        else:
+            raise AssertionError("Pin is not random")
+
+
